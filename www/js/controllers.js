f71fcd6b887f6e104f41ef0a1cd618e6c2291379
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-05-29 23:50:54 -0300

    add infinite scroll to jobs page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,21 @@ angular.module('starter.controllers', [])
 
 .controller('JobsCtrl', function($scope, Jobs) {
   $scope.jobs = Jobs.all();
+  $scope.page = 2;
+  $scope.moreItemsAvailable = true;
+
+  $scope.loadMore = function() {
+    var jobs = Jobs.all({page: $scope.page}, function(response) {
+      if (response.length > 0) {
+        $scope.jobs = $scope.jobs.concat(jobs);
+        $scope.page ++;
+      } else {
+        $scope.moreItemsAvailable = false;
+      }
+
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  }
 
   $scope.share = function(job) {
     console.log(job.id);
@@ -9,7 +24,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('JobViewCtrl', function($scope, $stateParams, Jobs) {
-  var job = Jobs.get({ id: $stateParams.id }, function(){
+  var job = Jobs.get({id: $stateParams.id}, function(){
     $scope.job = job;
   })
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,8 @@
 angular.module('starter.services', [])
 
 angular.module('starter').factory('Jobs', function($resource) {
-  return $resource('http://localhost:3000/api/v1/jobs/:id', { id: '@id' }, {
-    all: {
-      method: "GET",
-      isArray: true
-    },
-    show: {
-      method: "GET"
-    }
+  return $resource('http://localhost:3000/api/v1/jobs/:id', {id: '@id'}, {
+    all: {method: "GET", isArray: true},
+    show: {method: "GET"}
   })
 });

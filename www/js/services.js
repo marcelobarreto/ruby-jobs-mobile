bf246b4f74ec765133bf211a045dc43ae4cf7cd4
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-07-04 23:43:42 -0300

    Fix some problems with css

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('starter.services', [])
 
 angular.module('starter').factory('Jobs', function($resource) {
-  return $resource('http://ruby-jobs-api.herokuapp.com/api/v1/jobs/:id', {id: '@id'}, {})
+  return $resource('http://www.rubyjobs.me/api/v1/jobs/:id', {id: '@id'}, {})
 });
